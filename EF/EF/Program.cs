e1d3044296a62d9eba4309cabded6cdf6b405a78
Edvin <edvin-hakobyan@mail.ru>
2019-02-20 16:06:26 +0400

    EF add

---
+++ EF/EF/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+
+
+        }
+    }
+}
---
+++ SqlComand/SqlComand/Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlComand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //data source=(LocalDb)\MSSQLLocalDB;
+            //initial catalog=CodeFirst.Model;
+            //integrated security=True;
+            //MultipleActiveResultSets =True;
+
+            SqlConnectionStringBuilder SCSB = new SqlConnectionStringBuilder();
+            SCSB.DataSource = @"(LocalDb)\MSSQLLocalDB";
+            SCSB.InitialCatalog = "AdventureworksDW2016CTP3";
+            SCSB.IntegratedSecurity = false;
+            SCSB.Pooling = true;
+            //SCSB.UserID = "";
+            //SCSB.Password = "";
+
+
+            using (SqlConnection conection = new SqlConnection(SCSB.ConnectionString))
+            {
+                conection.Open();
+
+                string comand = @"SELECT * from dbo.FactFinance";
+
+                SqlCommand SQLcomand = new SqlCommand(comand, conection);
+
+                using (SqlDataReader DR = SQLcomand.ExecuteReader())
+                {
+
+                    while (DR.Read())
+                    {
+                        for (int i = 0; i < DR.FieldCount; i++)
+                        {
+                            Console.Write($"{DR.GetName(i)} ");
+                            Console.WriteLine($"'{DR[i]}' ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
